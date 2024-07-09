34cdc30e671a8adce2d9fd30d2c3ae73c0fcd9c8
Namitha Guruprasad <38079713+nam1410@users.noreply.github.com>
2024-07-09 14:52:02 -0400

    Update kde.py

--- romatch/utils/kde.py
+++ romatch/utils/kde.py
@@ -6,6 +6,7 @@ def kde(x, std = 0.1, half = True, down = None):
     if half:
         x = x.half() # Do it in half precision TODO: remove hardcoding
     if down is not None:
+        #ngurupr1 - computing the norm for gaussian kernel e^(-x^2/2*std^2); where std is the width of the kernel a.k.a inner scale (std>0)
         scores = (-torch.cdist(x,x[::down])**2/(2*std**2)).exp()
     else:
         scores = (-torch.cdist(x,x)**2/(2*std**2)).exp()
